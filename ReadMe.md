38121bb4d3b90f7cbe1a78ebba28df25b5188990
Ntsikayomzi Ngcakani <tsika1996190514@gmail.com>
2023-12-06 15:56:09 +0200

    Server Build 1st Attempt

--- ReadMe.md
+++ ReadMe.md
@@ -10,3 +10,19 @@ we can use to look at this is building a game of tic tac toe
 The components of this project consist of a live not so live server, a website
 with different variations of the game built using different methods, and lastly
 this read me file to guide viewers as to how this entire program works
+
+### Info:
+
+I have set the project to hold both my live server for those who wanna read 
+my code and a live version deployed on github. The github deployement must be
+incase I forget to do so in future.
+
+
+# Here We go
+
+## The Server
+
+I started of with a simple design using deconstructed methods to import my 
+necessary components. For the live server I had to figure out how to add an 
+automatic refresh. For the purpose of practicing I started building with concepts
+I know and understand
--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+//What I know
+
 /* Server imports */
 const { createServer } = require("http")
 const { createReadStream } = require("fs")
 const { join, extname } = require("path")
+
+/* Build 1 */
+
+createServer(( req, res ) => {
+
+    /* Resources required */
+    let public = join(__dirname, "public", req.url === "/"?"index.html":req.url)
+    let extension = extname(public)
+    let content = "text/html"
+
+    /* Switch case for different files */
+    switch(extension){
+        case '.css':
+            content = 'text/css'
+            break
+        case '.js':
+            content = 'text/javascript'
+            break
+        default:
+            content = 'text/html'
+            break
+    }
+
+    res.writeHead(200, {"Content-Type":content})
+    createReadStream(public).pipe(res)
+
+}).listen(4444)
+
 
